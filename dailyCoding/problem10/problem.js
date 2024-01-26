7808dd79e3fc5d628ef2fb7c5e340eaf90f4b73f
a-goaba <abdulrahman1997abdo@gmail.com>
2024-01-26 12:31:56 +0500

    solve task number 10 of the dailyCode

---
+++ dailyCoding/problem10/problem.js
@@ -0,0 +1,22 @@
+// This problem was asked by Apple.
+// Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+var JobScheduler = /** @class */ (function () {
+    function JobScheduler() {
+    }
+    JobScheduler.scheduleJob = function (f, n) {
+        if (n < 0) {
+            throw new Error("Invalid time duration. Please provide a non-negative value for 'n'.");
+        }
+        setTimeout(f, n);
+    };
+    return JobScheduler;
+}());
+var myFunction = function () {
+    console.log("Job executed!");
+};
+try {
+    JobScheduler.scheduleJob(myFunction, 2000);
+}
+catch (error) {
+    console.error(error.message);
+}
