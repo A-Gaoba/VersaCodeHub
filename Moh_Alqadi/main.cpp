0f01dae77afd7cae0809213a32118b28f3629001
a-goaba <abdulrahman1997abdo@gmail.com>
2024-02-01 21:22:51 +0500

    mohammed task for the first lab

--- Moh_Alqadi/main.cpp
+++ Moh_Alqadi/main.cpp
@@ -11,3 +11,16 @@ int main() {
 
   double result1 = sin(pow(x, 2)) - tan(pow(x, 1.0 / 3.0));
   cout << "Result 1: " << result1 << endl;
+
+  cout << "--------------------------" << endl;
+  cout << "----------task 2----------" << endl;
+
+  // task 2
+  double a = 1000;
+  double b = 0.0001;
+
+  double result2 = (pow((a - b), 3) - pow(a, 3)) / (3 * a * pow(b, 2) - pow(b, 3) - 3 * pow(a, 2) * b);
+  cout << "Result 2: " << result2 << endl;
+
+  return 0;
+}
