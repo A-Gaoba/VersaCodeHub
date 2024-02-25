707c2228d52277eeb0418f93bd64de81607deab4
a-goaba <abdulrahman1997abdo@gmail.com>
2024-02-25 20:00:01 +0500

    explanition to the arrays and muti multi Dimensional Arrays

--- Amro/array/arrays.cpp
+++ Amro/array/arrays.cpp
@@ -11,3 +11,20 @@ int main() {
   return 0;
 }
 
+// 2. Find the Maximum Element
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int array[5] = {1, 5, 3, 9, 2};
+  int max = array[0]; // Assume first element is the max initially
+  for (int i = 1; i < 5; i++) {
+    if (array[i] > max) {
+      max = array[i]; // Update max if current element is larger
+    }
+  }
+  cout << "Maximum Element = " << max;
+  return 0;
+}
+
