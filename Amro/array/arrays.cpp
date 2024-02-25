fc2788bacd11f41be8fd891916d4279a3d0251da
a-goaba <abdulrahman1997abdo@gmail.com>
2024-02-25 20:00:49 +0500

    explanition to the arrays and muti multi Dimensional Arrays

--- Amro/array/arrays.cpp
+++ Amro/array/arrays.cpp
@@ -74,3 +74,32 @@ int main() {
   }
   return 0;
 }
+
+
+// 5. Check Symmetry
+
+#include <iostream>
+using namespace std;
+
+// Function to check if the array is symmetric
+bool isSymmetric(int arr[], int n) {
+  for (int i = 0; i < n / 2; i++) {
+    if (arr[i] != arr[n - i - 1])
+      return false; // If elements don't match, it's not symmetric
+  }
+  return true; // If loop completes, array is symmetric
+}
+
+int main() {
+  int array[] = {1, 3, 5, 3, 1};
+
+  // Calling the function and checking the result
+  if (isSymmetric(array, sizeof(array) / sizeof(array[0]))) {
+    cout << "The array is symmetric.";
+  } else {
+    cout << "The array is not symmetric.";
+  }
+
+  return 0;
+}
+
