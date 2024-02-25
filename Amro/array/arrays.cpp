333ad78cf30b55cea5670482f8dee21b626e77a7
a-goaba <abdulrahman1997abdo@gmail.com>
2024-02-25 20:00:55 +0500

    explanition to the arrays and muti multi Dimensional Arrays

--- Amro/array/arrays.cpp
+++ Amro/array/arrays.cpp
@@ -103,3 +103,22 @@ int main() {
   return 0;
 }
 
+// 6. Matrix Addition
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int matrix1[2][2] = {{1, 2}, {3, 4}};
+  int matrix2[2][2] = {{5, 6}, {7, 8}};
+  int sum[2][2];
+
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      sum[i][j] = matrix1[i][j] + matrix2[i][j]; // Add elements
+      cout << sum[i][j] << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
