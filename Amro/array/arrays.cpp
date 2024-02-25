1e4c2ae4287f3291775e3594b99b6e47f27c42f1
a-goaba <abdulrahman1997abdo@gmail.com>
2024-02-25 19:59:45 +0500

    explanition to the arrays and muti multi Dimensional Arrays

---
+++ Amro/array/arrays.cpp
@@ -0,0 +1,13 @@
+// 1. Initialize a One-Dimensional Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int array[5] = {1, 2, 3, 4, 5}; // Initializing the array with values
+  for (int i = 0; i < 5; i++) {
+    cout << array[i] << " "; // Printing each element of the array
+  }
+  return 0;
+}
+
