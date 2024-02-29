5339e953ba8e9695982d10dd12baea9a934f962a
a-goaba <abdulrahman1997abdo@gmail.com>
2024-02-29 23:34:49 +0500

    solve task 17 of the DailyCode

---
+++ dailyCoding/problem18/problem.js
@@ -0,0 +1,36 @@
+// This problem was asked by Google.
+// Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.
+// For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7, 8, 8], since:
+// 10 = max(10, 5, 2)
+// 7 = max(5, 2, 7)
+// 8 = max(2, 7, 8)
+// 8 = max(7, 8, 7)
+// Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results. You can simply print them out as you compute them.
+function printMaxOfSubarrays(arr, k) {
+    var deque = []; // Holds indexes of useful elements
+    // Initial window
+    for (var i = 0; i < k; ++i) {
+        while (deque.length > 0 && arr[i] >= arr[deque.at(-1)]) {
+            deque.pop();
+        }
+        deque.push(i);
+    }
+    // Remaining windows
+    for (var i = k; i < arr.length; ++i) {
+        console.log(arr[deque[0]]); // Maximum of the previous window
+        // Remove indexes out of the current window
+        while (deque.length > 0 && deque[0] <= i - k) {
+            deque.shift();
+        }
+        // Remove smaller elements
+        while (deque.length > 0 && arr[i] >= arr[deque.at(-1)]) {
+            deque.pop();
+        }
+        deque.push(i);
+    }
+    console.log(arr[deque[0]]); // Maximum of the last window
+}
+// Example usage
+var arr = [10, 5, 2, 7, 8, 7];
+var k = 3;
+printMaxOfSubarrays(arr, k);
